0068703d914970f54f405b78e4041c3f0c40d7bc
wxseem-dev <sifat.miah403@gmail.com>
2026-07-18 12:31:28 +0100

    qubit state initialisation

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+# Normal computing, a bit is represented as a hard 0 (OFF) and 1 (ON)
+# In quantum computing, a qubit is in a mixed state between the two as a blend, mixture of 0 and 1
+# it could be pure 0 state so [1, 0] meaning 100% towards 0 or it could be pure 1 [0, 1] meaning 100% towards 1
+
+# we can use a state vector which is a list of numbers like above to represent how much 0 and how much 1 our qubit has
+qubit_state = np.array([1, 0]) # this represents our qubit as a pure 0 using numpy
