5cd69f783b51a2d009febdcebfc634fd110bd794
wxseem-dev <sifat.miah403@gmail.com>
2026-07-19 12:12:31 +0100

    coin flip using probabilities

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 # it could be pure 0 state so [1, 0] meaning 100% towards 0 or it could be pure 1 [0, 1] meaning 100% towards 1
 
 # we can use a state vector which is a list of numbers like above to represent how much 0 and how much 1 our qubit has
-qubit_state = np.array([1, 0]) # this represents our qubit as a pure 0 using numpy
+qubit_state = np.array([0, 1]) # this represents our qubit as a pure 0 using numpy
 
 print("qubit_state:", qubit_state)
 
@@ -15,10 +15,35 @@ print("qubit_state:", qubit_state)
 
 # in quantum mechanics the probability of measuring a state is the square of its value. and as 1/sqrt2 squared is 50% thats how we get our exact chance for both 0 and 1
 
+# we use the hadamard gate to get our 50/50 superposition
+
 hadamard_gate  = ((1/np.sqrt(2)) * np.array([[1, 1], [1, -1]]))
 print("hadamard gate:", hadamard_gate)
 
 
-altered_gate = hadamard_gate @ qubit_state
+altered_gate = hadamard_gate @ qubit_state # true matrix calculation is done with @
 
 print("superposition:", altered_gate)
+
+zero_state_probability = altered_gate[0] ** 2
+one_state_probability = altered_gate[1] ** 2
+
+# The superposition is the product/result of the multiplication, the state after the matrix is applied
+# a state vector after is just an index of options, so for a single qubit of [0,1] that has two slots
+# each slot represents the amplitude of 0 state and 1 state, e.g. how much of each
+# to find the probabilities, we square them individually
+# say we have a state vector of [0.8, 0.6]
+# the probabilities would be 0 = 0.8**2 = 0.64 (64% chance), 1 = 0.6**2 = 0.36 (36% chance)
+
+# the rule of quantum computing is that a qubit must collapse into either 0 or 1 so the squared probabilities
+# must add up to 1.0 or 100%
+
+choice = np.random.choice(a=[0,1], size=1000, replace=True, p=[zero_state_probability, one_state_probability])
+# choose out of 0 or 1, 1000 times each with its probability so its weighted
+
+
+count_0 = np.count_nonzero(choice == 0)
+count_1 = np.count_nonzero(choice == 1)
+
+print(count_0, '0s')
+print(count_1, '1s')
